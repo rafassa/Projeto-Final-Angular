cbba88a9bdde48df882ece6eea1e051ec0e8c889
rafassa <rafassa95@gmail.com>
2025-05-05 11:27:39 -0300

    barra de pesquisa semi finalização

---
+++ src/app/interfaces/Data.interface.ts
@@ -0,0 +1,9 @@
+export interface Data{
+
+    id: string,
+    odometro: string,
+    nivelCombustivel: string,
+    status:string,
+    lat: string,
+    long: string,
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,34 +1,30 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { AutenticacaoService } from '../services/autenticacao.service';
-import { FormsModule,  } from '@angular/forms';
-import { CommonModule, } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+import { NgIf } from '@angular/common';
 import { Router } from '@angular/router';
 import { Usuario } from '../interfaces/Usuario.interface';
 
-
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
-  service = inject(AutenticacaoService)
-  constructor( private router:Router){}
+ 
+  constructor(private service:AutenticacaoService, private router:Router){}
   mensagemDeErro:string | null=null
 
-
   
   
-
   addUser(user:Usuario[]){
-   
+    console.log(user)
     this.service.postAPI(user).subscribe({
       next: (data:Usuario[]) =>{
-        this.service.getApiInformation(data)
         this.router.navigateByUrl('/home')
-        
+        this.service.getApiInformation(data)
+       
       },
       error:(error)=> {
         this.mensagemDeErro=error.error.message
